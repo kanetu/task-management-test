658d0cde2becc3d1a9faf84bc715fc4e21f7ccee
Hieu Tu <hieu.t@eyepax.com>
2024-09-23 11:23:53 +0700

    remove storybook

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,4 +1,3 @@
-import Profile from "@features/Profile";
 import TaskManagement from "@features/TaskManagement";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "App.scss";
@@ -6,13 +5,9 @@ import React from "react";
 import {
   BrowserRouter,
   Navigate,
-  Outlet,
   Route,
-  Routes,
+  Routes
 } from "react-router-dom";
-import tw from "twin.macro";
-
-const Input = tw.input`border hover:border-blue-50 p-5 m-4`;
 
 const queryClient = new QueryClient();
 
@@ -21,25 +16,14 @@ const App: React.FC = () => {
     <QueryClientProvider client={queryClient}>
       <div className="main">
         <BrowserRouter>
-          {/* <Header /> */}
           <Routes>
-            <Route path="/task-management" element={<TaskManagement />} />
-            <Route path="/" element={<AuthRoute />}>
-              <Route path="/profile" element={<Profile />} />
-            </Route>
+            <Route path="/" element={<TaskManagement />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-          {/* <Footer /> */}
         </BrowserRouter>
       </div>
     </QueryClientProvider>
   );
-};
-
-const AuthRoute: React.FC = () => {
-  const isAuthenticated = true;
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default App;
